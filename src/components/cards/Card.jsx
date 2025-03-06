a660b1fff0c8410d56b010cd9082ebc3ce6d3b1e
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-03-07 00:31:54 +0500

    again for test

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -7,15 +7,15 @@ import Text2 from '../text/Text2'
 
 export default function Card({ item }) {
     return (
-        <div className='max-w-[390px] h-[450px] border border-[#E8E8EA] rounded-xl flex flex-col gap-4 items-center p-4 justify-between'>
-            <Link href={`/post/${item.id}`} >
-                <div className='overflow-hidden'>
+        <div className='lg:max-w-[390px] w-full h-[450px] border border-[#E8E8EA] rounded-xl flex flex-col gap-4 items-center p-4 justify-between'>
+            <Link href={`/post/${item.id}`} className='w-full'>
+                <div className='w-full overflow-hidden'>
                     <img
                         src={`${item.img}`}
                         alt={`card ${item.title}`}
-                        width={360}
-                        height={240}
-                        className='object-cover rounded-md transition duration-300 ease-in-out hover:scale-125 '
+                        // width={360}
+                        // height={240}
+                        className='w-full h-[240px] object-cover rounded-md transition duration-300 ease-in-out hover:scale-125 '
                     />
                 </div>
             </Link>
@@ -24,7 +24,7 @@ export default function Card({ item }) {
                 <h2 className={' font-semibold text-2xl leading-7'}>{item.title}</h2>
                 <div className='w-full flex items-center justify-between'>
                     <Link href={`/author/${item.user.name}`} className='flex items-center gap-3'>
-                        <Image src={`${item.user.img}`} alt={`card ${item.id}`} width={36} height={36   } className='rounded-full' />
+                        <img src={`${item.user.img}`} alt={`card ${item.id}`} width={36} height={36} className='rounded-full' />
                         <h4>{item.user.name}</h4>
                     </Link>
                     <h4>{item.user.date}</h4>
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 
 export default function Cards() {
     return (
-        <section className='my-10'>
+        <section className='my-10 lg:mx-0 mx-10'>
             <div className='w-full flex flex-wrap gap-5 lg:justify-between md:justify-center px-0 sm:px-5'>
                 {
                     CardData.map((item) => (
